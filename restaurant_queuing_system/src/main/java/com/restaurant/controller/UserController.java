12986d9f37638f1ef0d4b615152f45ecdd6dd865
jat <919700667@qq.com>
2015-08-26 23:28:34 +0800

    done

--- restaurant_queuing_system/src/main/java/com/restaurant/controller/UserController.java
+++ restaurant_queuing_system/src/main/java/com/restaurant/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.restaurant.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.JsonObject;
+import com.restaurant.service.UserService;
 
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping(value="/login")
 	@ResponseBody
--- restaurant_queuing_system/src/main/java/com/restaurant/service/impl/UserServiceImpl.java
+++ restaurant_queuing_system/src/main/java/com/restaurant/service/impl/UserServiceImpl.java
@@ -1,9 +1,15 @@
 package com.restaurant.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.restaurant.dao.UserMapper;
 import com.restaurant.service.UserService;
 @Service
 public class UserServiceImpl implements UserService{
 
+	@Autowired
+	private UserMapper userMapper;
+	
+	
 }
